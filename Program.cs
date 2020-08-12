580bb3aaf89e38241f7acb15199890bc0b1ef2b9
m7od99 <68038780+m7od99@users.noreply.github.com>
2020-08-12 11:49:48 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace I_love_Novri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I love Novri");
+        }
+    }
+}
